de5a0a8459a2810e839b93a1a0d4db707a905b18
limchanho <lchh8178@gmail.com>
2017-07-04 18:34:27 +0900

    170704

--- limchanho/src/common/DBConn2.java
+++ limchanho/src/common/DBConn2.java
@@ -11,30 +11,30 @@ public class DBConn2 {
 	public static Connection getCon() throws ClassNotFoundException, SQLException {
 		if (con == null) {
 			Class.forName("org.mariadb.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1/iot_test", "root", "rjqnrdl82");
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1/iot_test", "root", "sh1qk429");
 			con.setAutoCommit(false);
 		}
 		return con;
 	}
 
-	// public static void main(String[] args) {
-	// try {
-	// Connection con = DBConn2.getCon();
-	// System.out.println("접속 됬네잉!!" + con);
-	// DBConn2.closeCon();
-	//
-	// System.out.println("종료 됬다네잉~!" + con);
-	// } catch (Exception e) {
-	// e.printStackTrace();
-	// }
-	// }
+//	 public static void main(String[] args) {
+//	 try {
+//	 Connection con = DBConn2.getCon();
+//	 System.out.println("접속 됬네잉!!" + con);
+//	 DBConn2.closeCon();
+//	
+//	 System.out.println("종료 됬다네잉~!" + con);
+//	 } catch (Exception e) {
+//	 e.printStackTrace();
+//	 }
+//	 }
 
 	public static void closeCon() throws SQLException {
-		System.out.println(con);
+//		System.out.println(con);
 		if (con != null) {
 			con.close();
 			con = null;
 		}
-		System.out.println(con);
+//		System.out.println(con);
 	}
 }
--- limchanho/src/dao/BoardDAO.java
+++ limchanho/src/dao/BoardDAO.java
@@ -1,60 +1,89 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import common.DBConn2;
 
-public class BoardDAO {
+public class BoardDAO { //다외워
 	// board테이블에 update, insert, delete, select 하는 함수를 각각 4개 생성해주세요
-	// 모두 단일 작업임으로 한개의 함수마다 commit이 이러우져야 하며
-	// 만약 에러날경우 rollback이 되면 됩니다.
-	Connection con;
-	public void setConnection() throws ClassNotFoundException, SQLException{
-		con = DBConn2.getCon();
-	}
-	
-	public boolean insertBoard() throws SQLException{
-		String sql = "insert into board(title, content, writer, reg_Date)values('게시판제목3','게시판내용3',4,now())";
-		Statement st = con.createStatement();
-		int result = st.executeUpdate(sql);
-		if(result==1){
-			st.close();
-			st = null;
-			return true;
+		// 모두 단일 작업임으로 한개의 함수마다 commit이 이러우져야 하며
+		// 만약 에러날경우 rollback이 되면 됩니다.
+		Connection con;
+
+		public void setConnection() throws ClassNotFoundException, SQLException {
+			con = DBConn2.getCon();
 		}
-		return false;
-	}
-	
-	public boolean updateBoard() throws SQLException{
-		String sql = "update board1 set title='으하하하하' where num='1'";
-		Statement st = con.createStatement();
-		int result = st.executeUpdate(sql);
-		if(result==1){
-			st.close();
-			st = null;
-			return true;
-		}
-		return false;
-	}
-	
-	public static void main(String[] args){
-		BoardDAO bdao = new BoardDAO();
-		try {
-			bdao.setConnection();
-			bdao.insertBoard();
-			bdao.updateBoard();
-			bdao.con.commit();
-			System.out.println("정상동작 했고 저장까지 완료 했수다!!");
-		} catch (ClassNotFoundException | SQLException e) {
-			try {
-				bdao.con.rollback();
-				System.out.println("뭔진 모르겠는데 에러났어 그래서 원복했수다!!");
-			} catch (SQLException e1) {
-				e1.printStackTrace();
+
+		public boolean insertBoard() throws SQLException {
+			String sql = "insert into board(title, content, writer, reg_Date)values('게시판제목3','게시판내용3',4,now())";
+
+			Statement st = con.createStatement();
+			int result = st.executeUpdate(sql);
+			if (result == 1) {
+				st.close();
+				st = null;
+				return true;
 			}
-			e.printStackTrace();
+			return false;
+		}
+
+		public boolean updateBoard() throws SQLException {
+			String sql = "update board set title='으하하하하' where num='2'";
+
+			Statement st = con.createStatement();
+			int result = st.executeUpdate(sql);
+			if (result == 1) {
+				st.close();
+				st = null;
+				return true;
+			}
+			return false;
+		}
+
+		public List<Map> selectBoard() throws SQLException {
+			String sql = "select num, title, content, writer from board;";
+			PreparedStatement ps = con.prepareStatement(sql);
+
+			ResultSet rs = ps.executeQuery();
+			List boardList = new ArrayList();
+			while (rs.next()) {
+				HashMap hm = new HashMap();
+				hm.put("num", rs.getString("num"));
+				hm.put("title", rs.getString("title"));
+				hm.put("content", rs.getString("content"));
+				hm.put("writer", rs.getString("writer"));
+				boardList.add(hm);
+			}
+			rs.close();
+			rs = null;
+			ps.close();
+			ps = null;
+			return boardList;
+		}
+
+		public static void main(String[] args) {
+			BoardDAO bdao = new BoardDAO();
+			try {
+				bdao.setConnection();
+				List<Map> boardList = bdao.selectBoard();
+				CommentDAO dao = new CommentDAO();
+				for(Map m : boardList){
+					System.out.println(m);
+					List<Map> commentList = dao.getCommentList(Integer.parseInt((String)m.get("num")));
+					for(Map m2 : commentList){
+						System.out.println(m2);
+					}
+				}
+			} catch (ClassNotFoundException | SQLException e) {
+				e.printStackTrace();
+			}
 		}
 	}
-}
